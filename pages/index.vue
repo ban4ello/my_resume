d26d94d3d900894f24bbe8f04936566a56d5eaf6
ban4ello <ban4ellog@gmail.com>
2022-09-12 20:08:42 +0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="12" md="6">
+      <v-card>
+        <v-card-title class="headline ma-4">
+          Summary
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae eum dignissimos, vel accusantium laboriosam natus corporis amet dolores adipisci? Dolor a iste dolorem excepturi, officia ea consectetur? Molestias, laborum?
+        </v-card-text>
+      </v-card>
+
+      <v-card>
+        <v-card-title class="headline ma-4">
+          Technical skills
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae eum dignissimos, vel accusantium laboriosam natus corporis amet dolores adipisci? Dolor a iste dolorem excepturi, officia ea consectetur? Molestias, laborum?
+        </v-card-text>
+      </v-card>
+
+      <v-card>
+        <v-card-title class="headline ma-4">
+          Hobby
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae eum dignissimos, vel accusantium laboriosam natus corporis amet dolores adipisci? Dolor a iste dolorem excepturi, officia ea consectetur? Molestias, laborum?
+        </v-card-text>
+      </v-card>
+
+      <v-card>
+        <v-card-title class="headline ma-4">
+          Working experience
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae eum dignissimos, vel accusantium laboriosam natus corporis amet dolores adipisci? Dolor a iste dolorem excepturi, officia ea consectetur? Molestias, laborum?
+        </v-card-text>
+      </v-card>
+
+      <v-card>
+        <v-card-title class="headline ma-4">
+          Contacts
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae eum dignissimos, vel accusantium laboriosam natus corporis amet dolores adipisci? Dolor a iste dolorem excepturi, officia ea consectetur? Molestias, laborum?
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
